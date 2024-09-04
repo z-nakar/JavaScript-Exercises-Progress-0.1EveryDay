27147a432b786935d387cb57e26302fa544f5fec
z-nakar <xairelnakar01@gmail.com>
2024-09-04 12:07:02 +0800

    updated fixed

--- Exercises/01-01.js
+++ Exercises/01-01.js
@@ -2,7 +2,7 @@ function guessNumberGame(){
     let randomNumber = Math.floor(Math.random() * 10) + 1;
     let guessNumber = parseInt(prompt("Guess a number between 1 to 10"));
 
-    if(guessNumber < 10){
+    if(guessNumber < 11){
         if(guessNumber === randomNumber){
         alert("You have been blessed by the gods.");
             }else alert("You have been cursed with a very bad luck, the correct number is " + randomNumber + ".");
